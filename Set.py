f103fc0e0823c718c88495a5151b6e1bf845ce4d
Amanullah Rijve <sheikhrijve885@gmail.com>
2025-02-19 21:50:33 +0600

    Create Set.py

---
+++ Set.py
@@ -0,0 +1,22 @@
+# for no repet in program
+# make program uncangeable
+
+
+# empty set
+ rijve = set()
+ print(type(rijve))
+
+
+ set1= { 2, 4, 7,8 ,0}
+ set2= {4, 6, 8, 11, 2}
+
+
+ print(set2.intersection(set1))
+ print(set2.update(set1))
+ set_union= set1.union(set2)
+ print(set_union)
+
+
+set = {2, 7, 6, 9,}
+print(set)
+print(type(set))
