3f701bd00219031bc9150f5975ce68873c742a73
Amanullah Rijve <sheikhrijve885@gmail.com>
2025-02-19 21:49:31 +0600

    Create Doc_string.py

---
+++ Doc_string.py
@@ -0,0 +1,9 @@
+
+ def docType(a,b):
+     """ 
+     hi my name is amanullah rijve and i am 22 years old and i read in class 134 
+
+    """
+     sum = a+b
+    return sum
+ print(docType(2,4).__doc__)
