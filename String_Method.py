6d8ebefe60980adcb2f5c6a7e81ba1c3aa45d097
Amanullah Rijve <sheikhrijve885@gmail.com>
2025-02-19 21:42:48 +0600

    Create String_Method.py

---
+++ String_Method.py
@@ -0,0 +1,49 @@
+ a = "sheikh rijve"
+ print(a)
+ print(a.isalnum())
+
+ a= "I am a coder"
+ print(a)
+ print(a.index('am'))
+ print(a.index('I'))
+ print(a.index('a'))
+ print(a.index('coder'))
+
+ a= " wheren is my pen?"
+ print(a)
+ print(a.find('pen'))
+
+ a = "I want to live bangladesh"
+ b= "i love bangladesh?"
+ print(a)
+ print(a.endswith('?'))
+ print(b.endswith('?'))
+
+ a="my name is is is is rijve"
+ print(a)
+ print(a.count('is'))
+
+ a = "My name is rijve"
+ print(a)
+ print(a.center(60))
+
+ a='honesty is the best policy'
+ print(a)
+ print(a.capitalize())
+
+ a = 'sheikh rijve !!'
+ print(a.split())
+
+ a= 'rijve sheikh'
+ print(a)
+ print(a.replace('rijve','amanullah'))
+
+ veriable ='hello world i am rijve from pabna and i study in diu and i am a developer now i am learning python to be a ml dl '
+ print(veriable.rsplit())
+
+ name = 'MY NAME IS RIJVE'
+ print(name.lower())
+
+ a = "my name is rijve"
+ print(a)
+ print(a.upper())
