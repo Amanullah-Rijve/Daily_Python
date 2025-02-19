e41bbd9259350b77799e2f0ae0ae3a833ecff5ae
Amanullah Rijve <sheikhrijve885@gmail.com>
2025-02-19 21:48:42 +0600

    Create F_string.py

---
+++ F_string.py
@@ -0,0 +1,7 @@
+
+letter = "hey my name is {} and i am {} years old and I live in {}"
+ name ="Sheikh Rijve"
+ age= 22
+ country="Banglades"
+ print(letter.format(name,age,country))
+ print(f"hey my name is {name} and i am {age} years old and i live in {country}")
