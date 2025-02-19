8dc1714bd440a9c146eff7ba80960cbdcd10480d
Amanullah Rijve <sheikhrijve885@gmail.com>
2025-02-19 21:43:41 +0600

    Create If_Else.py

---
+++ If_Else.py
@@ -0,0 +1,10 @@
+
+ a= int(input("Enter Your Age: "))
+ print("Your Age is:",a)
+
+ if(a>18):
+     print("You can  marry")
+ elif(a==18):
+     print("You Can Marry if you are girl in Bangladesh")
+ else:
+     print("You Can not marry")
