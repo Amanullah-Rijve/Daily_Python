a5af53f48c9b38fd09047756d083e67611995fab
Amanullah Rijve <sheikhrijve885@gmail.com>
2025-02-19 21:54:53 +0600

    Create problem_1.py

---
+++ problem_1.py
@@ -0,0 +1,14 @@
+# ! make a code language programe
+
+ code = int(input("Enter code: "))
+
+ if(code==559):
+     print("I love you")
+ elif(code==999):
+     print("James Bond")
+ elif(code==773):   
+     print("Ans PLease")
+ elif(code==000):
+     print("Get lost")
+ else:
+    print(" Sorry, The Code You Entered is Not Included In This Programe ")
