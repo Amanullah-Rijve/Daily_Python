4fb46f5b501823612e23e44f8ba3b3d48879e508
Amanullah Rijve <sheikhrijve885@gmail.com>
2025-02-19 21:38:37 +0600

    Create UserInput.py

---
+++ UserInput.py
@@ -0,0 +1,13 @@
+ a=input("What is your name")
+ print("My name is",a)
+
+#  Lets use arithmatic operation taking inpur from user
+
+ a = input("Enter First number")
+ b = input("Enter sedcond number")
+
+ print("The Sum is:", int (a) + int(b) )
+ print("The Sub is:", int(a)-int(b))
+ print("The mul is:", int(a)*int(b) )
+ print("The Div is:", int(a)/int(b))
+ print("The mol is:", int(a)%int(b))
