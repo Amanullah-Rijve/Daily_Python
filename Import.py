4c968b7a37ba7e7b09e231607fbe28e4d3088c9f
Amanullah Rijve <sheikhrijve885@gmail.com>
2025-02-19 21:53:54 +0600

    Create Import.py

---
+++ Import.py
@@ -0,0 +1,29 @@
+# # First way
+
+import math 
+
+ result = math.sqrt(16)
+ print(result)
+
+# # Second way
+
+ from math import sqrt , pi
+
+ result = sqrt(25) * pi
+ print(result)
+
+# # 3rd and easy way
+
+from math import pow , sqrt as s 
+
+x = 25
+ result = s(x) * pow(x,2)
+ print(result) 
+
+# # another  example 
+
+ from math import pow, pi as rijve
+
+ r = 2.506
+ area = rijve * pow(r, 2)
+ print(area)
