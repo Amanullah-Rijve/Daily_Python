9f6db30123fca92d3ecb314982685df78c697e70
Amanullah Rijve <sheikhrijve885@gmail.com>
2025-02-19 21:52:22 +0600

    Create shortcut_if_else.py

---
+++ shortcut_if_else.py
@@ -0,0 +1,8 @@
+ a = 10
+ b = 12
+
+ print("Ami valo nie") if a>b else print("Ami Valo achi") if a==b else print("F  you")
+
+ a = int(input("Enter A INteger: "))
+
+ print("Hello") if a<1  else print("HI ther") if a==20 else print("Wlecome")
