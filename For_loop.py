6056de9bf59a0adac8d1a33afd3bf2587d1be4dc
Amanullah Rijve <sheikhrijve885@gmail.com>
2025-02-19 21:45:39 +0600

    Create For_loop.py

---
+++ For_loop.py
@@ -0,0 +1,37 @@
+
+ for i in range(1,11):
+    
+     print('1 X',i ,'=',1 * i) 
+     print('2 X',i ,'=',2 * i) 
+     print('3 X',i ,'=',3 * i)  
+     print('4 X',i ,'=',4 * i)  
+    print('5 X',i ,'=',5 * i)   
+    print('6 X',i ,'=',6 * i) 
+    print('7 X',i ,'=',7 * i)  
+    print('8 X',i ,'=',8 * i) 
+    print('9 X',i ,'=',9 * i)  
+   print('10 X',i ,'=',10 * i) 
+    
+     if(i==8):
+        break
+
+
+ for x in range(1,6,10):
+   print(x)
+
+
+ for x in range(1,6):
+  print(x)
+
+ pLan= ['Java','javascript','python','fothrun','C#','c++','rust','go']
+ for lan in pLan:
+     print(lan)
+     if(lan =='c#'):
+         break
+ 
+
+ fruits = ["apple", "banana", "cherry"]
+ for rijve in fruits:
+  print(rijve)
+  if (rijve == 'banana'):
+  break
