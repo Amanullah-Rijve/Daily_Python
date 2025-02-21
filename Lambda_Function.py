1be1da6f1233e1c7d5950320b5467dd2abc45695
Amanullah Rijve <sheikhrijve885@gmail.com>
2025-02-22 00:00:13 +0600

    Create Lambda_Function.py

---
+++ Lambda_Function.py
@@ -0,0 +1,17 @@
+# normal function
+def dui(a):
+    return a * 2
+
+print(dui(5))
+
+#! we use lambda function for mini function declaration
+
+#lambda function
+duii = lambda a: a*2
+
+print(duii(20))
+
+multiple = lambda a, b : (a * b)/2
+
+print(multiple(2,7))
+
