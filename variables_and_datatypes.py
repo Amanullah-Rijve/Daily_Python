cf130c369633474bd01c2869647e28905f3ebaa3
Amanullah Rijve <sheikhrijve885@gmail.com>
2025-02-23 23:19:10 +0600

    Create variables_and_datatypes.py

---
+++ variables_and_datatypes.py
@@ -0,0 +1,46 @@
+print(" Hello World ")
+
+print("Helllo World")
+
+
+
+basic syntax
+diu = "Rijve"
+print(diu) :
+
+#! variables in python
+
+first_name = "John"
+last_name = "John"
+ABC = "Daffodil"
+X = 2
+
+myVar = "name"
+my_var = "name"
+
+! rules of declaring variables 
+
+- 2  3 4, meaningfull, $
+
+# ! in C
+
+int var1 = 1
+float var2 = 1.5
+char var3 = "daffodil"
+double var = 12.354495448
+
+#! data types in python
+
+a = 1
+b = 1.5
+c = 'daffodil'
+
+print(type(a))
+
+x = int(1)
+y = float(1.5)
+z = str ('rijve')
+
+print(x)
+print(y)
+print(z)
