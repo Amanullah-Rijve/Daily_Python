3a6cc16e75bc40c4962d8464b1043af379767e78
Amanullah Rijve <sheikhrijve885@gmail.com>
2025-02-28 22:56:28 +0600

    Create list_basic.py

---
+++ list_basic.py
@@ -0,0 +1,87 @@
+# > < ==  (compare)
+
+#  || && (logical)
+
+# And gate ( && )
+# or gate ( || )
+
+# What And gate do? (" * ") 
+#  What a or Gate do? (" + ")
+
+# ! How to take USer input In Python
+
+# a = 1
+# input("Enter A Number: ")
+
+# print(a)
+
+# a = 2
+# b = 4
+
+# a = str(input("Enter a Float Number : "))
+
+# print(a)
+
+#!  Take input from user and print sum of two numbers
+
+# a , b = map(int(input("Enter Two Integer: "))) 
+
+# print(a+b)
+
+# a  = int(input("Enter First Integer: "))
+# b  = int(input("Enter second Integer: "))
+
+# print(a+b)
+
+# number_1 = float(input("Enter Number one: "))                                                                                                                            
+# number_2 = float(input("Enter Number two: "))  
+
+# print(number_1/number_2)
+
+# num_1 = float(input("Enter float number one: "))
+# num_2 = float(input("Enter float number two: "))
+# print(num_1 - num_2)
+
+#! Python List
+# list (keyword)
+# khbarer_talika = ['kacci','borhani','rice','pani','thand_pani']
+# print(list(khbarer_talika))
+# print(type(khbarer_talika))
+
+
+# name_of_fruits = ['mango','jackfruit','banana','apple','orange','coconut']
+# print(list(name_of_fruits))
+# print(type(name_of_fruits))
+
+#! what is a index number?
+
+# name_of_fruits = ['mango','jackfruit','banana','apple','orange','coconut']
+
+# name_of_fruits.remove("jackfruit")
+# name_of_fruits.append("angur")
+
+# name_of_fruits[5]= "Batabi_lebu"
+# print(list(name_of_fruits))
+
+# print(type(name_of_fruits))
+
+# for i in name_of_fruits:
+#     print(i)
+
+Daffodil_List = ['changaou', 'Ab4','Bonomaya','Kathal_tola','Hotasar_mor','Python_road','Amazone']
+
+# print(list(Daffodil_List))
+# print(len(Daffodil_List))  # lentgh method  = len()
+
+# Daffodil_List.remove("Bonomaya")       # I want to remove somthing by remove  mwthod
+Daffodil_List.append("Singara")
+print(list(Daffodil_List)) # we can add something  by append method
+print(Daffodil_List[2])
+
+for i in Daffodil_List:
+    print(i)
+
+
+
+
+
