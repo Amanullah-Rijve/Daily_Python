a8f99a39b3e7d36d83439caa5df6f05e30070f93
Amanullah Rijve <sheikhrijve885@gmail.com>
2025-03-02 16:46:19 +0600

    Create mcu.py

---
+++ mcu.py
@@ -0,0 +1,10 @@
+popular_mcu_char = ['Cap','Thor','Iron man','black widow','spider man']
+
+popular_mcu_char.remove("Iron man")
+popular_mcu_char.remove("black widow")
+
+
+popular_mcu_char.append("Thanos")
+popular_mcu_char.append("knul")
+
+print(popular_mcu_char)
