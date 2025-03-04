11b2fc24d2fc771657333f4ae5ec248a3172826f
Amanullah Rijve <sheikhrijve885@gmail.com>
2025-03-04 23:47:06 +0600

    Create RegEx.py

---
+++ RegEx.py
@@ -0,0 +1,12 @@
+
+import re
+
+ hello  = "Hello Bangladesh"
+ x =  re.search("@$$Bangladesh.dkfh hello.293", hello)
+
+ if x == hello :
+     print("Match Found")
+ else:
+     print("Not Found")
+
+
