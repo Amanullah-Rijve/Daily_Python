7b0319eb86e12ccda7a2c0c5715b869f658f19e2
Amanullah Rijve <sheikhrijve885@gmail.com>
2025-03-06 21:41:40 +0600

    Create Multipication_Table.py

---
+++ Multipication_Table.py
@@ -0,0 +1,69 @@
+print("Multipication table of 1 ")
+
+for i in range(1,11):
+    print(f"1 * {i} = {1 * i}")
+
+print("Multipication Tabla of 2")
+
+for i in range(1,11):
+     print(f"2 * {i} = {2 * i}")
+
+print()
+
+print("Multipication Tabla of 3")
+
+for i in range(1,11):
+     print(f"3 * {i} = {3 * i}")
+
+print()
+    
+print("Multipication Tabla of 4")
+
+for i in range(1,11):
+     print(f"4 * {i} = {4 * i}")
+
+print()
+
+print("Multipication Tabla of 5")
+
+for i in range(1,11):
+     print(f"5 * {i} = {5 * i}")
+
+print()
+
+print("Multipication Tabla of 6")
+
+for i in range(1,11):
+     print(f"6 * {i} = {6 * i}")
+
+print()
+
+print("Multipication Tabla of 7")
+
+for i in range(1,11):
+     print(f"7 * {i} = {7 * i}")
+
+print()
+
+print("Multipication Tabla of 8")
+
+for i in range(1,11):
+     print(f"8 * {i} = {8 * i}")
+
+print()
+
+print("Multipication Tabla of 9")
+
+for i in range(1,11):
+     print(f"9 * {i} = {9 * i}")
+
+print()
+
+print("Multipication Tabla of 10")
+
+for i in range(1,11):
+     print(f"10 * {i} = {10 * i}")
+
+print()
+
+
