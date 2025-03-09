6a3a3b85766aa633a5444a24aa499f70cb052f76
Amanullah Rijve <sheikhrijve885@gmail.com>
2025-03-09 21:14:03 +0600

    Create Centigrade.py

---
+++ Centigrade.py
@@ -0,0 +1,7 @@
+def temp(F):
+    C = float(5/9)*(F-32)
+    return C
+
+print(f"The Centigrade temparature is",temp(68),"c")
+print(f"The Centigrade temparature is",temp(75),"c")
+print(f"The Centigrade temparature is",temp(50),"c")
