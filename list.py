6b2209a029d200874e44fd88e488706c894fa0cb
Amanullah Rijve <sheikhrijve885@gmail.com>
2025-03-10 21:02:36 +0600

    Create list.py

---
+++ list.py
@@ -0,0 +1,6 @@
+mcu_characters = ['Wolverine','Iron_Man','Black_Widow','Thor','Captain_America']
+mcu_characters.remove('Black_Widow')
+mcu_characters.remove('Iron_Man')
+mcu_characters.append('Thanos')
+mcu_characters.append('Knul')
+print(mcu_characters) 
