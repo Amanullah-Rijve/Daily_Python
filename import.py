63c81cc68cf36447ddc7a2f0178f4cb0ccb2bf78
Amanullah Rijve <sheikhrijve885@gmail.com>
2025-03-13 23:43:11 +0600

    Create import.py

---
+++ import.py
@@ -0,0 +1,62 @@
+import math
+
+a = 25
+c = 49
+b = math.sqrt(a)
+d = math.sqrt(b)
+print(b)
+print(d)
+
+print(math.sqrt(25))
+
+a = 1.9
+
+print(math.floor(a))
+print(math.ceil(a))
+
+Floor = dicrease
+ceil = increase
+
+import random
+
+print(random.randint(7,8))
+
+how to customize module ?
+
+import math
+
+r = 2.5
+area = math.pi * (r * r)
+
+print(area)
+
+import math as machoman
+
+
+r = 2.5
+area = machoman.pi * (r * r)
+
+print(area)
+
+from math import pi as model
+
+r = float(input("Enter The Redius: "))
+
+area = model * (r * r)
+print(f"The Area Of Circle is: ",area)
+
+ fahreheit to centigrade
+
+import math as m
+
+f = float(input(" Enter Fahrenheit: "))
+
+c = 5/9 * (f-32)
+
+print(c)
+
+
+
+
+
+
