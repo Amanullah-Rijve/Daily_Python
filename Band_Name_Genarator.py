4c67e83bb627b7fcc6723c1262fab9ed7a055b9e
Md. Amanullah Rijve <sheikhrijve885@gmail.com>
2025-09-16 02:03:30 +0600

    Add files via upload

---
+++ Band_Name_Genarator.py
@@ -0,0 +1,7 @@
+print("Welcome To Band Name Genarator :)")
+cityName = input("What is the name of your city?\n")
+print(cityName)
+petName = input("What is your pet's name?\n")
+print(petName)
+bandName = cityName + " " + petName
+print(f"Your Band Name is {bandName}")
