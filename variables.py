a07ed0a6db778bf3b091b758090b4f4405fdd7bd
Md. Amanullah Rijve <sheikhrijve885@gmail.com>
2025-09-16 02:03:55 +0600

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,8 @@
+name = input("What is your name?")
+print(name)
+print(f"Your Name  has Total {len(name)} characters")
+
+userName= input("Enter Your NAme: ")
+lent = len(userName)
+print(userName)
+print(lent)
