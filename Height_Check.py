84f251311d361b67eaf4d1eab76b2c32c4c094ae
Md. Amanullah Rijve <sheikhrijve885@gmail.com>
2025-09-21 00:56:30 +0600

    Add files via upload

---
+++ Height_Check.py
@@ -0,0 +1,6 @@
+height = int(input("Enter Your Height in Integer: "))
+age = int(input("Enter ZYour Age: "))
+if(height>120 and age>18):
+    print("You Can Ride and you have to pay 12$")
+else:
+    print("Sorry! You Can't Ride")    
